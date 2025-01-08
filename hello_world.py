805d394da8dfbef2de52720e3051290966b5829b
Landon Harrison <harrisol27@up.edu>
2025-01-07 20:04:43 -0600

    LJH: (day5) added header, comments, and an introduction sentence

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,13 @@
-print("Hello, World!")
+'''
+This code outputs hello world as part of a 30 day python challenge
+
+Author: Landon Harrison
+Version: 1/7/2025
+'''
+print("\nHello, World!\n") # prints "Hello, World!" to the terminal and goes to next line
+ 
+#introducing myself
+introduction = "My name is Landon, I go to the University of Portland, "\
+"and as a kid I was obsessed with the show Supernatural"
+
+print(introduction+"\n")
